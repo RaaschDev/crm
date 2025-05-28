74c22312a5ea7e66c9964d824d2548c05e74c91b
Raphael <devraasch@gmail.com>
2025-05-28 18:47:13 -0300

    bug-fix: langchain-community

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d&o^9lwafe&bn37$d$#7(g@@w%mv8d+xv@y8e!xhx754qjb_e#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*", "localhost", "127.0.0.1", "10.124.0.3"]
 
 
 # Application definition
